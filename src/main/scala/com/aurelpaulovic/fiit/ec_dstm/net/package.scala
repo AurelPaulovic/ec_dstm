f55508537d50be97f7ae2fe9e5515916f7d8ee64
Aurel Paulovic <aurel.paulovic@gmail.com>
2013-11-27 13:38:18 +0100

    added check to context implicit conversion to throw up when trying to use null context

--- src/main/scala/com/aurelpaulovic/fiit/ec_dstm/net/package.scala
+++ src/main/scala/com/aurelpaulovic/fiit/ec_dstm/net/package.scala
@@ -5,7 +5,10 @@ import scala.pickling._
 import json._
 
 package object net {
-	implicit def context2RichContext(c: Context) = new RichContext(c)
+	implicit def context2RichContext(c: Context) = {
+	    if(c == null) throw new NullPointerException("calling implicit context2RichContext with null context")
+	    new RichContext(c)
+	}
 	
 	implicit def replyConnection2Addres(rc: ReplyConnection) = rc.getAddr
 	
